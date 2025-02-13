c6c01ab645bc7e45aee882995774e423e0444a9d
Jasmin Gubeljic <jasmin.g@tutanota.com>
2025-02-13 19:22:09 +0100

    feat: use router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>jQuiz</h1>,
+    id: "wrapperComponent",
+    children: [{ path: "/", element: <p>Quizzes</p> }],
+  },
+]);
 
 function App() {
-  return <>J-quiz</>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
